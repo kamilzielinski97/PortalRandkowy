53bfe941ea0a9cb3850eb18ae60b04bab527c934
Kamil Zieliński <gd40628@student.wsb.gda.pl>
2020-02-05 15:04:42 +0100

    aktualizacja tokenów

--- PortalRandkowy.SPA/src/app/app.component.ts
+++ PortalRandkowy.SPA/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './_services/auth.service';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'PortalRandkowy - SPA';
+export class AppComponent implements OnInit {
+
+  jwtHelper = new JwtHelperService();
+
+  constructor(private authServices: AuthService){}
+
+  ngOnInit(): void {
+    const token = localStorage.getItem('token');
+    if(token){
+      this.authServices.decoderToken = this.jwtHelper.decodeToken(token);
+    }
+  }
 }
--- PortalRandkowy.SPA/src/app/nav/nav.component.ts
+++ PortalRandkowy.SPA/src/app/nav/nav.component.ts
@@ -10,7 +10,7 @@ export class NavComponent implements OnInit {
 
   model: any = {};
 
-  constructor(private authService: AuthService) { }
+  constructor(public authService: AuthService) { }
 
   ngOnInit() {
   }
