f12df6a2458d9a9778537d9794aec9fcb8792b51
Kamil Zieliński <gd40628@student.wsb.gda.pl>
2020-02-10 20:19:44 +0100

    mozliwosc edytowania uzytkownika

--- PortalRandkowy.SPA/src/app/app.module.ts
+++ PortalRandkowy.SPA/src/app/app.module.ts
@@ -26,9 +26,11 @@ import { UserCardComponent } from './users/usere-list/user-card/user-card.compon
 import { UserDetailComponent } from './users/usere-list/user-detail/user-detail.component';
 import { UserDetailResolver } from './_resolvers/user-detail.resolver';
 import { UserListResolver } from './_resolvers/user-list.resolver';
+import { UserEditComponent } from './users/usere-list/user-edit/user-edit.component';
+import { UserEditResolver } from './_resolvers/user-edit.resolver';
 
 
-export function tokenGetter(){
+export function tokenGetter() {
    return localStorage.getItem('token');
 }
 
@@ -42,7 +44,8 @@ export function tokenGetter(){
       LikesComponent,
       MessagesComponent,
       UserCardComponent,
-      UserDetailComponent
+      UserDetailComponent,
+      UserEditComponent
    ],
    imports: [
       BrowserModule,
@@ -68,7 +71,8 @@ export function tokenGetter(){
        AuthGuard,
        ErrorInterceptorProvider,
        UserDetailResolver,
-       UserListResolver
+       UserListResolver,
+       UserEditResolver
    ],
    bootstrap: [
       AppComponent
