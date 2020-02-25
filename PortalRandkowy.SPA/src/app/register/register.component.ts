8903a2231fbb18ebc20b554119bfd1d8985da2a8
Kamil Zieliński <gd40628@student.wsb.gda.pl>
2020-02-25 14:16:46 +0100

    Nowy formularz rejestracyjny

--- PortalRandkowy.SPA/src/app/register/register.component.ts
+++ PortalRandkowy.SPA/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -11,21 +12,28 @@ export class RegisterComponent implements OnInit {
 
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
+  registerForm: FormGroup;
 
   constructor(private authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
-  }
-
-  register(){
-    this.authService.register(this.model).subscribe(() => {
-      this.alertify.success('rejestracja udana');
-    }, error => {
-      this.alertify.error(error);
+    this.registerForm = new FormGroup({
+      username: new FormControl(),
+      password: new FormControl(),
+      confirmPassword: new FormControl(),
     });
   }
 
-  cancel(){
+  register() {
+    // this.authService.register(this.model).subscribe(() => {
+    //   this.alertify.success('rejestracja udana');
+    // }, error => {
+    //   this.alertify.error(error);
+    // });
+    console.log(this.registerForm.value);
+  }
+
+  cancel() {
     this.cancelRegister.emit(false);
   }
 
