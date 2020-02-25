d6598a69e5e4893f5cdd59f68aeebb0202038ed8
Kamil Zieliński <gd40628@student.wsb.gda.pl>
2020-02-25 13:21:26 +0100

    usuwanie zdjec API

--- PortalRandkowy.API/Controllers/PhotosController.cs
+++ PortalRandkowy.API/Controllers/PhotosController.cs
@@ -118,5 +118,39 @@ namespace PortalRandkowy.API.Controllers
             return BadRequest("Nie można ustawić zdjęcia jako głównego");
         }
 
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePhoto(int userId, int id)
+        {
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+
+            var user = await _repository.GetUser(userId);
+
+            if (!user.Photos.Any(p => p.Id == id))
+                return Unauthorized();
+
+            var photoFromRepo = await _repository.GetPhoto(id);
+
+            if (photoFromRepo.IsMain)
+                return BadRequest("Nie można usunąć zdjecia głównego");
+
+            if (photoFromRepo.public_id != null)
+            {            
+                var deleteParams = new DeletionParams(photoFromRepo.public_id);
+                var result = _claudinary.Destroy(deleteParams);
+
+                if (result.Result == "ok")
+                    _repository.Delate(photoFromRepo);
+
+            }
+            
+            if (photoFromRepo.public_id == null)
+                _repository.Delate(photoFromRepo);
+
+            if (await _repository.SaveAll())
+                return Ok();
+            
+            return BadRequest("Nie udało sie usunąć zdjecia");
+        }
     }
 }
