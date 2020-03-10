c65d42c79e355f8ecbb1d32e4af72d24a48e2648
Kamil Zieliński <gd40628@student.wsb.gda.pl>
2020-03-10 18:42:47 +0100

    funkcja polubien api

--- PortalRandkowy.API/Data/IUserRepository.cs
+++ PortalRandkowy.API/Data/IUserRepository.cs
@@ -12,5 +12,6 @@ namespace PortalRandkowy.API.Data
          Task<Photo> GetPhoto(int id);
 
          Task<Photo> GetMainPhotoForUser(int userId);
+         Task<Like> GetLike(int userId, int recipientId);
     }
 }
