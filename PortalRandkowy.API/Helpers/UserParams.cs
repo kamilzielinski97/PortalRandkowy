e4d401e1ea9edb6496e790e8bb4d17c829ce31e0
Kamil Zieliński <gd40628@student.wsb.gda.pl>
2020-03-10 19:02:46 +0100

    pobieranie list lubiacych uzytkownikow

--- PortalRandkowy.API/Helpers/UserParams.cs
+++ PortalRandkowy.API/Helpers/UserParams.cs
@@ -15,6 +15,8 @@ namespace PortalRandkowy.API.Helpers
         public int MinAge { get; set; } = 18;
         public int MaxAge { get; set; } = 100;
         public string ZodiacSign { get; set; } = "Wszystkie";
-        public string OrderBy { get; set; }   
+        public string OrderBy { get; set; }
+        public bool UserLikes { get; set; } = false;
+        public bool UserIsLiked { get; set; } = false;   
     }
 }
