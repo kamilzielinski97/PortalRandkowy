fd5267eee5f055c637d2b25bd2c2bcf6b52dba5b
Kamil Zieliński <gd40628@student.wsb.gda.pl>
2020-03-11 16:03:53 +0100

    metody dla skrzynki nadawczej i odbiorczej

--- PortalRandkowy.API/Data/IUserRepository.cs
+++ PortalRandkowy.API/Data/IUserRepository.cs
@@ -10,11 +10,10 @@ namespace PortalRandkowy.API.Data
          Task<PagedList<User>> GetUsers(UserParams userParams);
          Task<User> GetUser(int id);
          Task<Photo> GetPhoto(int id);
-
          Task<Photo> GetMainPhotoForUser(int userId);
          Task<Like> GetLike(int userId, int recipientId);
          Task<Message> GetMessage(int id);
-         Task<PagedList<Message>> GetMessagesForUser();
+         Task<PagedList<Message>> GetMessagesForUser(MessageParams messageParams);
          Task<IEnumerable<Message>> GetMessageThread(int userId, int recipientId);
     }
 }
