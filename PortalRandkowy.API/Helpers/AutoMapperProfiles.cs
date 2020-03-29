dfd93b00facca2d419e21ba89b7d6981d25f6cb8
Kamil Zieliński <gd40628@student.wsb.gda.pl>
2020-03-29 18:31:43 +0200

    skrzynka nadawcza

--- PortalRandkowy.API/Helpers/AutoMapperProfiles.cs
+++ PortalRandkowy.API/Helpers/AutoMapperProfiles.cs
@@ -10,18 +10,18 @@ namespace PortalRandkowy.API.Helpers
         public AutoMapperProfiles()
         {
             CreateMap<User, UserForListDto>()
-            .ForMember(dest => dest.PhotoUrl, opt => {
-                opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url);
+                .ForMember(dest => dest.PhotoUrl, opt => {
+                    opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url);
             })
-            .ForMember(dest => dest.Age, opt => {
-                opt.ResolveUsing(src => src.DateOfBirth.CalculateAge());
+                .ForMember(dest => dest.Age, opt => {
+                    opt.ResolveUsing(src => src.DateOfBirth.CalculateAge());
             });
             CreateMap<User, UserForDetailedDto>()
-            .ForMember(dest => dest.PhotoUrl, opt => {
-                opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url);
+                .ForMember(dest => dest.PhotoUrl, opt => {
+                 opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url);
             })
-            .ForMember(dest => dest.Age, opt => {
-                opt.ResolveUsing(src => src.DateOfBirth.CalculateAge());
+                .ForMember(dest => dest.Age, opt => {
+                    opt.ResolveUsing(src => src.DateOfBirth.CalculateAge());
             });
             CreateMap<Photo, PhotosForDetailedDto>();
             CreateMap<UserForUpdateDto, User>();
@@ -29,6 +29,9 @@ namespace PortalRandkowy.API.Helpers
             CreateMap<PhotoForCreationDto, Photo>();
             CreateMap<UserForRegisterDto, User>();
             CreateMap<MessageForCreationDto, Message>().ReverseMap();
+            CreateMap<Message, MessageToReturnDto>()
+                .ForMember(m => m.SenderPhotoUrl, opt => opt.MapFrom(u => u.Sender.Photos.FirstOrDefault(p => p.IsMain).Url));
+
         }
     }
 }
